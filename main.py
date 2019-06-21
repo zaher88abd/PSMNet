bb7f88657962109f87487e1740dc8a8984c5ae0a
Zaher <zaher88abd@gmail.com>
2019-06-21 12:16:41 -0300

    read dataset

--- main.py
+++ main.py
@@ -45,7 +45,6 @@ if args.cuda:
     torch.cuda.manual_seed(args.seed)
 
 all_left_img, all_right_img, all_left_disp, test_left_img, test_right_img, test_left_disp = lt.dataloader(args.datapath)
-
 TrainImgLoader = torch.utils.data.DataLoader(
     DA.myImageFloder(all_left_img, all_right_img, all_left_disp, True),
     batch_size=12, shuffle=True, num_workers=8, drop_last=False)
